601e22bbf79a817e3d5105c2b2cb91b0c6583006
hpphpro <Fullaurum@gmail.com>
2024-06-14 05:26:28 +0300

    full refactor

--- .venv/lib/python3.12/site-packages/aiogram/types/giveaway_created.py
+++
@@ -1,9 +0,0 @@
-from .base import TelegramObject
-
-
-class GiveawayCreated(TelegramObject):
-    """
-    This object represents a service message about the creation of a scheduled giveaway. Currently holds no information.
-
-    Source: https://core.telegram.org/bots/api#giveawaycreated
-    """
